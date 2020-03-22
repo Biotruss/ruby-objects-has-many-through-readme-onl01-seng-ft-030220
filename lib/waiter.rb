21780f9e918f4bdcf063e7d19e86a8e0b457404d
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-22 03:19:39 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -21,8 +21,12 @@ class Waiter
   end
   
   def best_tipper
-    meals.all.each {|meal| meal.customer == self}
+  best_tipped_meal = meals.max do |meal_a, meal_b|
+    meal_a.tip <=> meal_b.tip
   end
+ 
+  best_tipped_meal.customer
+end
   
   
   
